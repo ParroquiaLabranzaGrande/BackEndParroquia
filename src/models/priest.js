d6c958a9fe95d7f4a5e9de02e99b82aec8ecbad7
Lauragranados2021 <laura.granados05@uptc.edu.co>
2024-10-14 15:03:12 -0500

    model priest

---
+++ src/models/priest.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  documentNumber: {
+    type: String,
+    required: true
+  },
+  typeDocument: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'DocumentType',
+    required: true,
+  },
+  mail: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  Calendar: {
+    type: String,
+    required: true,
+  },
+  resetCode: String,
+  resetCodeExpires: Date
+});
+
+
+module.exports = mongoose.model('Priest', userSchema);
